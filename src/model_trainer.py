ad4164f18b8ebe2749d1d2dba8a68c84dad02233
Juyeong Ji <bebechien@users.noreply.huggingface.co>
2025-12-17 02:04:33 +0000

    Add tags in modelcard

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -87,7 +87,7 @@ def upload_model_to_hub(folder_path: Path, repo_name: str, token: str) -> str:
             repo_id=repo_id,
             token=token
         )
-        tags = info.tags
+        tags = info.card_data.tags
         tags.append("embeddinggemma-modkit")
         metadata_update(repo_id, {"tags": tags}, overwrite=True, token=token)
         
